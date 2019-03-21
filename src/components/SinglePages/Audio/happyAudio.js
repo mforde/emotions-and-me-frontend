7012cf2bea0e24269139dccc50e9fcc1022519ad
Egbantan Babatunde <egbantanbabatunde@Egbantans-MacBook-Pro.local>
2019-03-21 10:04:15 -0400

    happy audio not playing, fix backgrouad

--- src/components/SinglePages/Audio/happyAudio.js
+++ src/components/SinglePages/Audio/happyAudio.js
@@ -1,13 +1,120 @@
-import React from 'react';
+import React, { Component } from 'react'
+import AudioPlayer from 'react-playlist-player'
 
-export  class happyAudioPlayer extends React.Component {
-  render() {
-        return (
-        <div>
-          <audio controls>
-              <source src="https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/BrowseAP/Angry/audio/1001_DFA_ANG_XX.wav"  type="audio/mpeg"/>
-          </audio>
-      </div>
+export  class happyAudioPlayer extends Component {
+  state = {
+    currentPlayList: {}
+  }
+
+  loadPlayList = () =>
+    this.setState({
+      currentPlayList: {
+        playlistCoverUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Pictures/HAPPY/9.jpeg',
+        playlistName: 'HAPPY Playlist',
+        songs: [
+          {
+            position: '1',
+            songName: '1',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/FEAR/100.wav'
+          },
+          {
+            position: '2',
+            songName: '2',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/10.wav'
+          },
+          {
+            position: '3',
+            songName: '3',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1000.wav'
+          },
+          {
+            position: '4',
+            songName: '4',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/104.wav'
+          },
+          {
+            position: '5',
+            songName: '5',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1005.wav'
+          },
+          {
+            position: '6',
+            songName: '6',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1006.wav'
+          },
+          {
+            position: '7',
+            songName: '7',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1007.wav'
+          },
+          {
+            position: '8',
+            songName: '8',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1008.wav'
+          },
+          {
+            position: '9',
+            songName: '9',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1009.wav'
+          },
+          {
+            position: '10',
+            songName: '10',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1010.wav'
+          },
+          {
+            position: '11',
+            songName: '11',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1011.wav'
+          },
+          {
+            position: '12',
+            songName: '12',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1012.wav'
+          },
+          {
+            position: '13',
+            songName: '13',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1013.wav'
+          },
+          {
+            position: '14',
+            songName: '14',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1014.wav'
+          },
+          {
+            position: '15',
+            songName: '15',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1015.wav'
+          },
+          {
+            position: '16',
+            songName: '16',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1016.wav'
+          },
+          {
+            position: '17',
+            songName: '17',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1017.wav'
+          },
+          {
+            position: '18',
+            songName: '18',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/HAPPY/1018.wav'
+          }
+        ],
+        type: 'album'
+      }
+    })
+
+    render() {
+      return (
+        <div className={'Demo'}>
+          <button className={'Demo__load-btn'} onClick={this.loadPlayList}>
+            load playlist
+          </button>
+          <AudioPlayer currentPlayList={this.state.currentPlayList} onToggle={({audioPlaying}) => console.log({audioPlaying})}/>
+        </div>
     )
   }
 }
--- src/components/SinglePages/Audio/neutralAudio.js
+++ src/components/SinglePages/Audio/neutralAudio.js
@@ -1,13 +1,120 @@
-import React from 'react';
+import React, { Component } from 'react'
+import AudioPlayer from 'react-playlist-player'
 
-export  class neutralAudioPlayer extends React.Component {
-  render() {
-        return (
-        <div>
-          <audio controls>
-              <source src="https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/BrowseAP/Angry/audio/1001_DFA_ANG_XX.wav"  type="audio/mpeg"/>
-          </audio>
-      </div>
+export  class neutralAudioPlayer extends Component {
+  state = {
+    currentPlayList: {}
+  }
+
+  loadPlayList = () =>
+    this.setState({
+      currentPlayList: {
+        playlistCoverUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Pictures/NEUTRAL/4.jpeg',
+        playlistName: 'Angry Playlist',
+        songs: [
+          {
+            position: '1',
+            songName: '1',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/100.wav'
+          },
+          {
+            position: '2',
+            songName: '2',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/10.wav'
+          },
+          {
+            position: '3',
+            songName: '3',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1000.wav'
+          },
+          {
+            position: '4',
+            songName: '4',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/104.wav'
+          },
+          {
+            position: '5',
+            songName: '5',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1005.wav'
+          },
+          {
+            position: '6',
+            songName: '6',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1006.wav'
+          },
+          {
+            position: '7',
+            songName: '7',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1007.wav'
+          },
+          {
+            position: '8',
+            songName: '8',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1008.wav'
+          },
+          {
+            position: '9',
+            songName: '9',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1009.wav'
+          },
+          {
+            position: '10',
+            songName: '10',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1010.wav'
+          },
+          {
+            position: '11',
+            songName: '11',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1011.wav'
+          },
+          {
+            position: '12',
+            songName: '12',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1012.wav'
+          },
+          {
+            position: '13',
+            songName: '13',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1013.wav'
+          },
+          {
+            position: '14',
+            songName: '14',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1014.wav'
+          },
+          {
+            position: '15',
+            songName: '15',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1015.wav'
+          },
+          {
+            position: '16',
+            songName: '16',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1016.wav'
+          },
+          {
+            position: '17',
+            songName: '17',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1017.wav'
+          },
+          {
+            position: '18',
+            songName: '18',
+            songUrl: 'https://s3.us-east-2.amazonaws.com/emotions-and-me-bucket/Audio/NEUTRAL/1018.wav'
+          }
+        ],
+        type: 'album'
+      }
+    })
+
+    render() {
+      return (
+        <div className={'Demo'}>
+          <button className={'Demo__load-btn'} onClick={this.loadPlayList}>
+            load playlist
+          </button>
+          <AudioPlayer currentPlayList={this.state.currentPlayList} onToggle={({audioPlaying}) => console.log({audioPlaying})}/>
+        </div>
     )
   }
 }
