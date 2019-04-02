769b8b9ad1afb12ac0cc35cefd81ccd9fba9681b
Cassie <cassiels636@gmail.com>
2019-04-02 14:19:48 -0400

    Added try again and emojis

--- src/components/QuizResults.js
+++ src/components/QuizResults.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../App.css';
+import emojis from "../constants/Emojis";
 
 function Result(props) {
-    return (
-        <div className="result w3-center w3-padding-64 w3-large">
-            Congratulations! You got <strong>{props.numCorrect}</strong> out of {props.numQuestions} questions right!
-        </div>
-    );
+
+    if ((props.numCorrect / props.numQuestions) >= 0.5) {
+        return (
+            <div className="result w3-center w3-padding-64">
+                <h2 className="w3-padding-large">Congratulations! You got <strong>{props.numCorrect}</strong> out
+                    of {props.numQuestions} questions right!</h2>
+                <div className="buttons">
+                    <button className="w3-button w3-theme w3-margin w3-padding" onClick={() => window.location.reload()}>Try Again?</button>
+                    <a href="/">
+                        <button className="w3-button w3-theme w3-margin w3-padding">Go Home</button>
+                    </a>
+                </div>
+                <img className="w3-image w3-center w3-padding-top" src={emojis.party} alt="party" height="50"
+                     style={{width: '50%'}}/>
+            </div>
+        )
+    } else {
+        return (
+            <div className="result w3-center w3-padding-64">
+                <h2 className="w3-padding-large">You got <strong>{props.numCorrect}</strong> out
+                    of {props.numQuestions} questions right!</h2>
+                <div className="buttons">
+                    <button className="w3-button w3-theme w3-margin w3-padding" onClick={() => window.location.reload()}>Try Again?</button>
+                    <a href="/">
+                        <button className="w3-button w3-theme w3-margin w3-padding">Go Home</button>
+                    </a>
+                </div>
+            </div>
+        )
+    }
 }
 
 Result.propTypes = {
