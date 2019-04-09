00fe8ceb2d9d9f457e6eb632e2375e514ca7f78b
Cassie <cassiels636@gmail.com>
2019-04-08 20:48:14 -0400

    Added Homepage Icons

--- src/components/FeatureGrid.js
+++ src/components/FeatureGrid.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 import {connect} from "react-redux";
+import featureIcons from "../constants/featureIcons";
 
 class FeatureGrid extends Component {
     featureGrid() {
@@ -10,41 +11,16 @@ class FeatureGrid extends Component {
                     <div className="w3-container">
                         <h1 className="w3-center">Emotions & Me</h1>
                         <div className="w3-row-padding w3-center w3-margin-top">
-                            <a href="/webcam">
+                            <a href="/myassignments">
                                 <div className="w3-third">
                                     <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray w3-white">
-                                        <h3>Emotions on Your Face</h3>
-                                        <p className="w3-padding-top">Using your webcam, we will tell you</p>
-                                        <p>what emotion you are expressing</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="/videostreaming">
-                                <div className="w3-third">
-                                    <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray w3-white">
-                                        <h3>Emotions on Their Faces</h3>
-                                        <p className="w3-padding-top">Choose a youtube video and we will tell you</p>
-                                        <p>what emotions the people in the video are expressing</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="/recordaudio">
-                                <div className="w3-third">
-                                    <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray w3-white">
-                                        <h3>Emotions in Your Voice</h3>
-                                        <p className="w3-padding-top">Record yourself and we will tell you</p>
-                                        <p>what emotion you are expressing with your voice</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="w3-row-padding w3-center w3-margin-top">
-                            <a href="/browse">
-                                <div className="w3-third">
-                                    <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray w3-white">
-                                        <h3>Emotions in Photos & Audio</h3>
-                                        <p className="w3-padding-top">Look through photos and audio clips of</p>
-                                        <p>people expressing all different kinds of emotions</p>
+                                        <h3>My Assignments</h3>
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.assignments}
+                                            alt={"assignments"}
+                                            style={{width: '35%'}}
+                                        />
                                     </div>
                                 </div>
                             </a>
@@ -52,17 +28,72 @@ class FeatureGrid extends Component {
                                 <div className="w3-third">
                                     <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray w3-white">
                                         <h3>Assignment Creator</h3>
-                                        <p className="w3-padding-top">Create quizzes and tasklists and send them</p>
-                                        <p>to your students to practice understanding emotions</p>
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.assignmentcreator}
+                                            alt={"assignment-creator"}
+                                            style={{width: '35%'}}
+                                        />
                                     </div>
                                 </div>
                             </a>
-                            <a href="/myassignments">
+                            <a href="/browse">
                                 <div className="w3-third">
                                     <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray w3-white">
-                                        <h3>My Assignments</h3>
-                                        <p className="w3-padding-top">Here's a list of assignments that</p>
-                                        <p>your teacher has created and sent to you</p>
+                                        <h3>Emotions in Photos & Audio</h3>
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.photos}
+                                            alt={"photos"}
+                                            style={{width: '35%'}}
+                                        />
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.audio}
+                                            alt={"audio"}
+                                            style={{width: '35%'}}
+                                        />
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="w3-row-padding w3-center w3-margin-top">
+                            <a href="/webcam">
+                                <div className="w3-third">
+                                    <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray w3-white">
+                                        <h3>Emotions on Your Face</h3>
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.webcam}
+                                            alt={"webcam"}
+                                            style={{width: '35%'}}
+                                        />
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="/videostreaming">
+                                <div className="w3-third">
+                                    <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray w3-white">
+                                        <h3>Emotions on Their Faces</h3>
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.videostreaming}
+                                            alt={"video-streaming"}
+                                            style={{width: '50%'}}
+                                        />
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="/recordaudio">
+                                <div className="w3-third">
+                                    <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray w3-white">
+                                        <h3>Emotions in Your Voice</h3>
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.record}
+                                            alt={"record"}
+                                            style={{width: '35%'}}
+                                        />
                                     </div>
                                 </div>
                             </a>
@@ -74,12 +105,50 @@ class FeatureGrid extends Component {
                     <div className="w3-container">
                         <h1 className="w3-center">Emotions & Me</h1>
                         <div className="w3-row-padding w3-center w3-margin-top">
+                            <a href="/myassignments">
+                                <div style={{width: '33.33333%'}} className="w3-show-inline-block">
+                                    <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray">
+                                        <h3>My Assignments</h3>
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.assignments}
+                                            alt={"assignments"}
+                                            style={{width: '35%'}}
+                                        />
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="/browse">
+                                <div style={{width: '33.33333%'}} className="w3-show-inline-block">
+                                    <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray">
+                                        <h3>Emotions in Photos & Audio</h3>
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.photos}
+                                            alt={"photos"}
+                                            style={{width: '35%'}}
+                                        />
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.audio}
+                                            alt={"audio"}
+                                            style={{width: '35%'}}
+                                        />
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="w3-row-padding w3-center w3-margin-top">
                             <a href="/webcam">
                                 <div className="w3-third">
                                     <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray">
                                         <h3>Emotions on Your Face</h3>
-                                        <p className="w3-padding-top">Using your webcam, we will tell you</p>
-                                        <p>what emotion you are expressing</p>
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.webcam}
+                                            alt={"webcam"}
+                                            style={{width: '35%'}}
+                                        />
                                     </div>
                                 </div>
                             </a>
@@ -87,8 +156,12 @@ class FeatureGrid extends Component {
                                 <div className="w3-third">
                                     <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray">
                                         <h3>Emotions on Their Faces</h3>
-                                        <p className="w3-padding-top">Choose a youtube video and we will tell you</p>
-                                        <p>what emotions the people in the video are expressing</p>
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.videostreaming}
+                                            alt={"video-streaming"}
+                                            style={{width: '50%'}}
+                                        />
                                     </div>
                                 </div>
                             </a>
@@ -96,28 +169,12 @@ class FeatureGrid extends Component {
                                 <div className="w3-third">
                                     <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray">
                                         <h3>Emotions in Your Voice</h3>
-                                        <p className="w3-padding-top">Record yourself and we will tell you</p>
-                                        <p>what emotion you are expressing with your voice</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div className="w3-row-padding w3-center w3-margin-top">
-                            <a href="/browse">
-                                <div style={{width: '33.33333%'}} className="w3-show-inline-block">
-                                    <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray">
-                                        <h3>Emotions in Photos & Audio</h3>
-                                        <p className="w3-padding-top">Look through photos and audio clips of</p>
-                                        <p>people expressing all different kinds of emotions</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="/myassignments">
-                                <div style={{width: '33.33333%'}} className="w3-show-inline-block">
-                                    <div className="w3-card w3-container w3-hover-shadow w3-hover-light-gray">
-                                        <h3>My Assignments</h3>
-                                        <p className="w3-padding-top">Here's a list of assignments that</p>
-                                        <p>your teacher has created and sent to you</p>
+                                        <img
+                                            className="w3-image w3-margin w3-padding-top"
+                                            src={featureIcons.record}
+                                            alt={"record"}
+                                            style={{width: '35%'}}
+                                        />
                                     </div>
                                 </div>
                             </a>
@@ -125,8 +182,7 @@ class FeatureGrid extends Component {
                     </div>
                 )
             }
-        }
-        else {
+        } else {
             return (
                 <div className="w3-container">
                     <h1 className="w3-center">Emotions & Me</h1>
